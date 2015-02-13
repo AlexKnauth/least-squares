3a62320ba6722c245b4873e6712cc014fc422a42
AlexKnauth <alexander@knauth.org>
2015-02-13 13:14:36 -0500

    least-squares/function-struct.rkt to reprovide stuff

---
+++ least-squares/function-struct.rkt
@@ -0,0 +1,12 @@
+#lang racket/base
+
+(provide (all-from-out
+          "function-struct/multi-var-taylor-ish.rkt"
+          "function-struct/vector-function.rkt"
+          "function-struct/function-struct.rkt"
+          ))
+
+(require "function-struct/multi-var-taylor-ish.rkt"
+         "function-struct/vector-function.rkt"
+         "function-struct/function-struct.rkt"
+         )
