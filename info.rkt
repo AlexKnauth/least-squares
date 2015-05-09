06477b1b0b02a44ae0b5d2524503b3617ffacb17
AlexKnauth <alexander@knauth.org>
2015-05-08 21:35:59 -0400

    fix deps

--- info.rkt
+++ info.rkt
@@ -3,7 +3,10 @@
 (define collection 'multi)
 
 (define deps '("base" "math-lib" "sweet-exp" "postfix-dot-notation" "my-cond"
-                      "git://github.com/AlexKnauth/infix-macro"))
+                      "kw-utils" "plot-gui-lib" "plot-lib" "unstable-list-lib"
+                      "git://github.com/AlexKnauth/infix-macro"
+                      ))
 
-(define build-deps '("rackunit-lib"))
+(define build-deps '("rackunit-lib"
+                     ))
 
